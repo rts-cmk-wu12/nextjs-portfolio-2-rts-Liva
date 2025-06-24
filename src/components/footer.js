8cca8bb65b9d341c760492370baf546f334cdf24
rts-Liva <U2451012@rts-365.dk>
2025-06-24 08:50:37 +0200

    use custom colour

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="bg-[#1B1B1B] py-4 fixed bottom-0 inset-x-0">
-            <p className="text-center text-sm text-neutral-500">Made with ❤️</p>
+            <p className="text-center text-sm text-[#9C9C9C]">Made with ❤️</p>
         </footer>
     );
 }
